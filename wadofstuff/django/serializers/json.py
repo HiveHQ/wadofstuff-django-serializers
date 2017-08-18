e32b95ace420642125e8bc05fa76851649966bc0
Martin Gingras <martin@mgingras.ca>
2017-08-18 16:10:18 -0400

    simplejson not supported in django >= 1.7

--- wadofstuff/django/serializers/json.py
+++ wadofstuff/django/serializers/json.py
@@ -1,17 +1,20 @@
 """
 Serialize data to/from JSON
 """
-from django.utils import simplejson
+import json
+
 from python import Serializer as PythonSerializer
 from django.core.serializers.json import Deserializer as JSONDeserializer, \
     DjangoJSONEncoder
+
+
 class Serializer(PythonSerializer):
     """
     Convert a queryset to JSON.
     """
     def end_serialization(self):
         """Output a JSON encoded queryset."""
-        simplejson.dump(self.objects, self.stream, cls=DjangoJSONEncoder,
+        json.dump(self.objects, self.stream, cls=DjangoJSONEncoder,
             **self.options)
 
     def getvalue(self):
